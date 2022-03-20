0e86999248689d5f76dace3ddbbee5995116cc03
S. Evens JOSEPH <84663627+evensdev@users.noreply.github.com>
2022-03-20 18:44:29 +0100

    Update brutforce.py

--- brutforce.py
+++ brutforce.py
@@ -1,41 +1,34 @@
 import itertools
 
-# importation du package itertools permettant de générer toutes les combinaisons
-# création d'un liste avec les actions et leurs taux d'intérêts
-
 actions = [
-    {'ref': 'Action-1', 'coût': 20, 'taux': 5},
-    {'ref': 'Action-2', 'coût': 30, 'taux': 10},
-    {'ref': 'Action-3', 'coût': 50, 'taux': 15},
-    {'ref': 'Action-4', 'coût': 70, 'taux': 20},
-    {'ref': 'Action-5', 'coût': 60, 'taux': 17},
-    {'ref': 'Action-6', 'coût': 80, 'taux': 25},
-    {'ref': 'Action-7', 'coût': 22, 'taux': 7},
-    {'ref': 'Action-8', 'coût': 26, 'taux': 11},
-    {'ref': 'Action-9', 'coût': 48, 'taux': 13},
-    {'ref': 'Action-10', 'coût': 34, 'taux': 27},
-    {'ref': 'Action-11', 'coût': 42, 'taux': 17},
-    {'ref': 'Action-12', 'coût': 110, 'taux': 9},
-    {'ref': 'Action-13', 'coût': 38, 'taux': 23},
-    {'ref': 'Action-14', 'coût': 14, 'taux': 1},
-    {'ref': 'Action-15', 'coût': 18, 'taux': 3},
-    {'ref': 'Action-16', 'coût': 8, 'taux': 8},
-    {'ref': 'Action-17', 'coût': 4, 'taux': 12},
-    {'ref': 'Action-18', 'coût': 10, 'taux': 14},
-    {'ref': 'Action-19', 'coût': 24, 'taux': 21},
-    {'ref': 'Action-20', 'coût': 114, 'taux': 18},
+    {'ref': 'Action-1', 'coût': 20, 'benefice': 5},
+    {'ref': 'Action-2', 'coût': 30, 'benefice': 10},
+    {'ref': 'Action-3', 'coût': 50, 'benefice': 15},
+    {'ref': 'Action-4', 'coût': 70, 'benefice': 20},
+    {'ref': 'Action-5', 'coût': 60, 'benefice': 17},
+    {'ref': 'Action-6', 'coût': 80, 'benefice': 25},
+    {'ref': 'Action-7', 'coût': 22, 'benefice': 7},
+    {'ref': 'Action-8', 'coût': 26, 'benefice': 11},
+    {'ref': 'Action-9', 'coût': 48, 'benefice': 13},
+    {'ref': 'Action-10', 'coût': 34, 'benefice': 27},
+    {'ref': 'Action-11', 'coût': 42, 'benefice': 17},
+    {'ref': 'Action-12', 'coût': 110, 'benefice': 9},
+    {'ref': 'Action-13', 'coût': 38, 'benefice': 23},
+    {'ref': 'Action-14', 'coût': 14, 'benefice': 1},
+    {'ref': 'Action-15', 'coût': 18, 'benefice': 3},
+    {'ref': 'Action-16', 'coût': 8, 'benefice': 8},
+    {'ref': 'Action-17', 'coût': 4, 'benefice': 12},
+    {'ref': 'Action-18', 'coût': 10, 'benefice': 14},
+    {'ref': 'Action-19', 'coût': 24, 'benefice': 21},
+    {'ref': 'Action-20', 'coût': 114, 'benefice': 18}
 ]
 
-# Initialisation des gains maximums
 max_gain = 0
-
-# Initialisation de la variable contenant les meilleures combinaisons
 best_combination = None
-
-# Initialisation de la taille des combinaisons
 taille = 0
+depense = 0
+taux_cumul = 0
 
-# Création d'une boucle permettant d'effectuer toutes les combinaisons en respectant les contraintes de coûts et de rentabilité maximums
 for taille in range(2, len(actions) + 1):
 
     for combination in itertools.combinations(actions, taille):
@@ -44,14 +37,23 @@ for taille in range(2, len(actions) + 1):
 
         for element in combination:
             total_price += element['coût']
-            total_gain += element['coût'] * element["taux"] / 100
+            total_gain += element['coût'] * element["benefice"] / 100
 
         if (total_price <= 500 and total_gain > max_gain):
             max_gain = total_gain
             best_combination = combination
 
-# Affichage de la rentabilité maximal obtenue en Euros
-# Affichage des combinaisons permettant d'obtenir le resultat. Il permet de dépenser 498€  d'actions et gagner 99,01 € en 2 ans
+for i in range(len(best_combination)):
+    depense += best_combination[i]['coût']
+    taux_cumul += best_combination[i]['benefice']
 
-print('Gain:', round(max_gain, 1), "€")
-print('Combinaison rentable:', best_combination)
+taux_moyen = round(taux_cumul / len(best_combination))
+
+print("Montant dépensé :", depense, "€")
+print("Nb d'actions : ", len(best_combination))
+print("taux d'intérêt moyen :", round(taux_cumul / len(best_combination)), "%")
+print("Gains :", taux_moyen)
+
+for i in range(len(best_combination)):
+    print(best_combination[i]['ref'])
+
